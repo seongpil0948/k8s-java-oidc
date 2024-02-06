3501f544cd941c0249b760cbc733a8413d0b3273
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2024-02-06 12:14:40 +0900

    feat: init github oidc for example

--- src/main/java/abacus/oidc/github/IndexController.java
+++ src/main/java/abacus/oidc/github/IndexController.java
@@ -1,4 +1,4 @@
-package abacus.oidc.sample1;
+package abacus.oidc.github;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
--- src/main/java/abacus/oidc/github/security/SecurityConfiguration.java
+++ src/main/java/abacus/oidc/github/security/SecurityConfiguration.java
@@ -1,10 +1,9 @@
-package abacus.oidc.sample1.security;
+package abacus.oidc.github.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
